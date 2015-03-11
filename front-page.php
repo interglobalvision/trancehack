2df233c46e4e87a0874f3137f4028718d8fbf7ed
mikey <michaelrayvon@gmail.com>
2015-03-11 16:36:34 -0600

    adds image bg and image overly to front page

--- front-page.php
+++ front-page.php
@@ -12,9 +12,16 @@ if( have_posts() ) {
     $video_bg = esc_html(get_post_meta( $post->ID, '_igv_video_bg', true ));
     $image_overlay = get_post_meta( $post->ID, '_igv_image_overlay', true );
 ?>
-<?php if ($video_bg) {
+<?php 
+if ($image_bg) {
+  echo '<div class="front-page-bg" style="background-image: url('.$image_bg.')"></div>';
+}
+if ($video_bg) {
   $video_embed_code = wp_oembed_get($video_bg, array( 'autoplay' => 1, 'modestbranding' => 1, 'showinfo' => 0, 'loop' => 1, 'title' => 0, 'portrait' => 0) ); 
   echo $video_embed_code;
+}
+if ($image_overlay) {
+  echo '<div class="front-page-overlay" style="background-image: url('.$image_overlay.')"></div>';
 }
 ?>
 <?php
