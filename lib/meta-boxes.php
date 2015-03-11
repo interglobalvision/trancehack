0b2b2cf9a0ee75c606728bcab149eae989c70410
Patrick Best <patrickbest@patrickbest.com>
2015-03-11 13:34:06 -0600

    adds gif post type and post and gif metaboxes
    but metabox doing weird shit tho with post types

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -1,18 +1,18 @@
 <?php
 
-/* Get post objects for select field options */ 
+/* Get post objects for select field options */
 function get_post_objects( $query_args ) {
-$args = wp_parse_args( $query_args, array(
-    'post_type' => 'post',
-) );
-$posts = get_posts( $args );
-$post_options = array();
-if ( $posts ) {
+  $args = wp_parse_args( $query_args, array(
+      'post_type' => 'post',
+    ) );
+  $posts = get_posts( $args );
+  $post_options = array();
+  if ( $posts ) {
     foreach ( $posts as $post ) {
-        $post_options [ $post->ID ] = $post->post_title;
+      $post_options [ $post->ID ] = $post->post_title;
     }
-}
-return $post_options;
+  }
+  return $post_options;
 }
 
 
@@ -31,13 +31,59 @@ return $post_options;
 add_action( 'cmb2_init', 'igv_cmb_metaboxes' );
 function igv_cmb_metaboxes() {
 
-	// Start with an underscore to hide fields from custom fields list
-	$prefix = '_igv_';
+  // Start with an underscore to hide fields from custom fields list
+  $prefix = '_igv_';
 
-	/**
-	 * Metaboxes declarations here
+  /**
+   * Metaboxes declarations here
    * Reference: https://github.com/WebDevStudios/CMB2/blob/master/example-functions.php
-	 */
+   */
+
+  $post_metaboxes = new_cmb2_box( array(
+      'id'            => $prefix . 'metabox',
+      'title'         => __( 'Post Settings', 'cmb2' ),
+      'object_types'  => array( 'post', ), // Post type
+      'context'       => 'normal',
+      'priority'      => 'high',
+      'show_names'    => true, // Show field names on the left
+    )
+  );
+
+  $post_metaboxes->add_field( array(
+      'name'       => __( 'Soundcloud', 'cmb2' ),
+      'desc'       => __( 'soundcloud URL', 'cmb2' ),
+      'id'         => $prefix . 'soundcloud',
+      'type'       => 'text',
+    )
+  );
+
+  $post_metaboxes->add_field( array(
+      'name'       => __( 'Vimeo', 'cmb2' ),
+      'desc'       => __( 'vimeo ID [only the numbers in the URL]', 'cmb2' ),
+      'id'         => $prefix . 'vimeo',
+      'type'       => 'text',
+    )
+  );
+
+  $gif_metaboxes = new_cmb2_box( array(
+      'id'            => $prefix . 'metabox',
+      'title'         => __( 'Gif', 'cmb2' ),
+      'object_types'  => array( 'gif', ), // Post type
+      'context'       => 'normal',
+      'priority'      => 'high',
+      'show_names'    => true, // Show field names on the left
+    )
+  );
+
+  $gif_metaboxes->add_field( array(
+      'name'       => __( 'THE GIF', 'cmb2' ),
+      'desc'       => __( ' ', 'cmb2' ),
+      'id'         => $prefix . 'gif',
+      'type'       => 'file',
+    )
+  );
 
 }
+
+
 ?>
--- lib/post-types.php
+++ lib/post-types.php
@@ -1,59 +1,61 @@
 <?php
 // Menu icons for Custom Post Types
+/*
 function add_menu_icons_styles(){
 ?>
- 
+
 <style>
 #adminmenu .menu-icon-project div.wp-menu-image:before {
     content: '\f498';
 }
 </style>
- 
+
 <?php
 }
 add_action( 'admin_head', 'add_menu_icons_styles' );
+*/
 
 
 //Register Custom Post Types
-add_action( 'init', 'register_cpt_project' );
+add_action( 'init', 'register_cpt_gif' );
 
-function register_cpt_project() {
+function register_cpt_gif() {
 
-    $labels = array( 
-        'name' => _x( 'Projects', 'project' ),
-        'singular_name' => _x( 'Project', 'project' ),
-        'add_new' => _x( 'Add New', 'project' ),
-        'add_new_item' => _x( 'Add New Project', 'project' ),
-        'edit_item' => _x( 'Edit Project', 'project' ),
-        'new_item' => _x( 'New Project', 'project' ),
-        'view_item' => _x( 'View Project', 'project' ),
-        'search_items' => _x( 'Search Projects', 'project' ),
-        'not_found' => _x( 'No projects found', 'project' ),
-        'not_found_in_trash' => _x( 'No projects found in Trash', 'project' ),
-        'parent_item_colon' => _x( 'Parent Project:', 'project' ),
-        'menu_name' => _x( 'Projects', 'project' ),
+    $labels = array(
+        'name' => _x( 'Gifs', 'gif' ),
+        'singular_name' => _x( 'Gif', 'gif' ),
+        'add_new' => _x( 'Add New', 'gif' ),
+        'add_new_item' => _x( 'Add New Gif', 'gif' ),
+        'edit_item' => _x( 'Edit Gif', 'gif' ),
+        'new_item' => _x( 'New Gif', 'gif' ),
+        'view_item' => _x( 'View Gif', 'gif' ),
+        'search_items' => _x( 'Search Gifs', 'gif' ),
+        'not_found' => _x( 'No gifs found', 'gif' ),
+        'not_found_in_trash' => _x( 'No gifs found in Trash', 'gif' ),
+        'parent_item_colon' => _x( 'Parent Gif:', 'gif' ),
+        'menu_name' => _x( 'Gifs', 'gif' ),
     );
 
-    $args = array( 
+    $args = array(
         'labels' => $labels,
         'hierarchical' => false,
-        
-        'supports' => array( 'title', 'editor', 'thumbnail' ),
-        
+
+        'supports' => array( 'title', 'thumbnail' ),
+
         'public' => true,
         'show_ui' => true,
         'show_in_menu' => true,
         'menu_position' => 5,
-        
+
         'show_in_nav_menus' => true,
         'publicly_queryable' => true,
         'exclude_from_search' => false,
-        'has_archive' => true,
+        'has_archive' => false,
         'query_var' => true,
         'can_export' => true,
         'rewrite' => true,
         'capability_type' => 'post'
     );
 
-    register_post_type( 'project', $args );
+    register_post_type( 'gif', $args );
 }
