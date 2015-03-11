0fcd16ec40978ace6ebf3c967bdf41962df877c6
mikey <michaelrayvon@gmail.com>
2015-03-11 17:04:28 -0600

    adds gif popover js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 jQuery(document).ready(function () {
   'use strict';
-  console.log('Hola Globie');
+  
+  $('.js-gif-trigger').on('click', function() {
+  	var gifsrc = $(this).attr('data-gif');
+  	if ($('#gif-popover').length) {
+  		$('#gif-popover').css('background-image','url('+gifsrc+')');
+  	} else {
+  		$('#main-container').append('<div id="gif-popover" style="background-image: url('+gifsrc+')"></div>');
+  	}
+  	$('#gif-popover').on('click', function() {
+  		$(this).remove();
+	  });
+  });
+
+
+
 });
