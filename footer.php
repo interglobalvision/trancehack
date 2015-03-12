d68297567b8fddb9eb8c897ac8d218a8edf66d42
mikey <michaelrayvon@gmail.com>
2015-03-11 18:20:41 -0600

    adds codez

--- footer.php
+++ footer.php
@@ -5,5 +5,16 @@
 
   <?php get_template_part('partials/scripts'); ?>
 
+  <?php 
+  if (is_tax( 'artist' )) { 
+	  $artist_id = get_queried_object()->term_id;
+	  $codez = Taxonomy_MetaData::get( 'artist', $artist_id, '_igv_codez');
+	  if ($codez) {
+		echo $codez;
+	  }
+	}
+	?>
+
+
   </body>
 </html>
